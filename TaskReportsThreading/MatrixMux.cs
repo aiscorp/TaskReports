a4428931166b5d780b6d13322db6f7a783400d61
Ivan Alenin <aiscorp@yandex.ru>
2019-10-12 12:57:15 +0300

    Заготовки класса умножения матриц

--- TaskReportsThreading/MatrixMux.cs
+++ TaskReportsThreading/MatrixMux.cs
@@ -8,14 +8,54 @@ using System.Threading.Tasks;
 
 namespace TaskReportsThreading
 {
-    class MatrixMux
+    public class MatrixMux
     {
 
-        static void ParallelMatrixMux()
+        //Замеры времени, не реализовано пока
+        TimeSpan timeSpan;
+        DateTime _startTS;
+        DateTime _endTS;
+
+        // Матрицы        
+        int[,] matrixA;
+        int[,] matrixB;
+        int[,] matrixC;
+
+        int matrixLength;
+
+        Random rnd = new Random();
+
+        // конструктор без параметров
+        public MatrixMux()
         {
+            // Создаем матрицы, по заданию на 100*100
+            matrixLength = 100;
+            int[,] matrixA = new int[matrixLength, matrixLength];
+            int[,] matrixB = new int[matrixLength, matrixLength];            
 
-
+            // Заполнение матриц случайными числами (0-255)
+            Random rnd = new Random();
+            for (int i = 0; i < matrixLength; i++)
+                for (int j = 0; j < matrixLength; j++)
+                {
+                    matrixA[i, j] = rnd.Next(0, 256);
+                    matrixB[i, j] = rnd.Next(0, 256);
+                }
         }
+
+        public int[,] ParallelMatrixMux(int[,] matrixA, int[,] matrixB)
+        {
+            _startTS = DateTime.Now;
+
+            // Умножение матриц
+
+            
+            timeSpan = DateTime.Now - _startTS;
+            
+            return matrixC;            
+        }
+
+
 
 
 
--- TaskReportsThreading/Program.cs
+++ TaskReportsThreading/Program.cs
@@ -1,14 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
+
 namespace TaskReportsThreading
 {
     class Program
     {
+
+        static void Main(string[] args)
+        {
+            // При работе с матрицей будем выводить результат в файл
+            StreamWriter outputFile = new StreamWriter("log.txt");
+
+
+
+            MatrixMux matrixMux = new MatrixMux();
+
+
+            //int[,] matrixC = matrixMux.ParallelMatrixMux();
+
+
+
+            Console.ReadKey();
+
+
+
+        }
+
+
+        
 
 
 
